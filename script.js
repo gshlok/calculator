483507009bc206ca05d269e91eb5175a7ef51b60
Shlok <shlokgraman@gmail.com>
2025-04-13 17:17:53 +0530

    calc() function built using eval(). basic calculator finished

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 let d = "";
 let buttons = document.querySelectorAll("button");
+
+function calc(str) {
+    for(var i = 0 ; i < str.length ; i++) {
+        if(str[i] == "×") str = str.replace("×", "*");
+        if(str[i] == "÷") str = str.replace("÷", "/");
+    }
+    return eval(str).toString();
+}
+
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         if(button.innerText == "DEL") d = d.slice(0, -1);
-        if(button.innerText == "AC") {d = "";}
+        else if(button.innerText == "AC") d = "";
+        else if(button.innerText == "=") d = calc(d);
         else d += button.innerText;
 
 
